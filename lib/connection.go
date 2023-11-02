6e96298c884be80aec64c0ba30bb22a227998457
tristam-deriv <tristam@deriv.com>
2023-11-02 09:05:05 +0000

    Added delay in both directions, and debug output

--- lib/connection.go
+++ lib/connection.go
@@ -6,8 +6,8 @@ import (
 	"io"
 	"net"
 	"strings"
+    //"encoding/hex"
 	"time"
-
 	"github.com/hashicorp/go-hclog"
 )
 
@@ -20,7 +20,8 @@ type connection struct {
 	srcConn, destConn io.ReadWriteCloser
 	bufferSize        int
 	latencyGen        LatencyGenerator
-	delayQueue        chan transitBuffer
+	delayQueueSrc     chan transitBuffer
+	delayQueueDest    chan transitBuffer
 	done              chan error
 	ctx               context.Context
 	log               hclog.Logger
@@ -45,33 +46,44 @@ func (c *connection) readFromSrc() {
 		}
 
 		c.log.Trace("Writing to delay queue", "bytes", bytes, "delay", desiredLatency)
+		//c.log.Warn("Writing to delay queue", "hex", hex.EncodeToString(buffer[0:bytes]))
+		c.log.Warn("Writing to delay queue", "str", string(buffer[0:bytes]))
 
-		c.delayQueue <- t
+		c.delayQueueSrc <- t
 
 	}
 }
 
 func (c *connection) readFromDest() {
-	buffer := make([]byte, c.bufferSize)
 	for {
+		buffer := make([]byte, c.bufferSize)
 		bytes, err := c.destConn.Read(buffer)
+		receivedAt := time.Now()
 		if err != nil {
-			c.done <- fmt.Errorf("Error reading data from proxy destination: %s", err)
+			c.done <- fmt.Errorf("Error reading data from server %s", err)
 			return
 		}
 		trimmedBuffer := buffer[:bytes]
+		desiredLatency := c.latencyGen.generateLatency(receivedAt)
+		delayUntil := receivedAt.Add(desiredLatency)
 
-		bytes, err = c.srcConn.Write(trimmedBuffer)
-		if err != nil {
-			c.done <- fmt.Errorf("Error writing data back to proxy client: %s", err)
-			return
+		t := transitBuffer{
+			data:       trimmedBuffer,
+			delayUntil: delayUntil,
 		}
+
+		c.log.Trace("Writing to dest delay queue", "bytes", bytes, "delay", desiredLatency)
+		//c.log.Warn("Writing to delay queue", "hex", hex.EncodeToString(buffer[0:bytes]))
+		c.log.Warn("Writing to dest delay queue", "str", string(buffer[0:bytes]))
+
+		c.delayQueueDest <- t
+
 	}
 }
 
-func (c *connection) readFromDelayQueue() {
+func (c *connection) readFromDelayQueueSrc() {
 	for {
-		t := <-c.delayQueue
+		t := <-c.delayQueueSrc
 
 		c.log.Trace("Read from delay queue", "bytes", len(t.data))
 
@@ -85,6 +97,22 @@ func (c *connection) readFromDelayQueue() {
 	}
 }
 
+func (c *connection) readFromDelayQueueDest() {
+	for {
+		t := <-c.delayQueueDest
+
+		c.log.Trace("Read from delay queueDest", "bytes", len(t.data))
+
+		time.Sleep(time.Until(t.delayUntil))
+
+		_, err := c.srcConn.Write(t.data)
+		if err != nil {
+			c.done <- fmt.Errorf("Error writing from delay queue to proxy Dest: %s", err)
+			return
+		}
+	}
+}
+
 // start launches 3 goroutines responsible for handling a proxy connection
 // (dest->src, src->queue, queue->dest). This operation will block until
 // either an error is sent via the done channel or the context is cancelled.
@@ -92,7 +120,8 @@ func (c *connection) start() {
 	c.log.Debug("Starting a new proxy connection")
 	go c.readFromDest()
 	go c.readFromSrc()
-	go c.readFromDelayQueue()
+	go c.readFromDelayQueueSrc()
+	go c.readFromDelayQueueDest()
 	for {
 		select {
 		case err := <-c.done:
@@ -143,7 +172,8 @@ func newProxyConnection(
 		destConn:   destConn,
 		bufferSize: bufferSize,
 		latencyGen: latencyGen,
-		delayQueue: make(chan transitBuffer, queueSize),
+		delayQueueSrc: make(chan transitBuffer, queueSize),
+		delayQueueDest: make(chan transitBuffer, queueSize),
 		done:       make(chan error, 3),
 		ctx:        ctx,
 		log:        logger,
